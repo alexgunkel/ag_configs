15c1b4c93bb4b97cf6e0bce242cb4a4cc7446b92
Alexander Gunkel <alexandergunkel@gmail.com>
2016-11-21 23:39:29 +0100

    v8-compatibility

--- Classes/Controller/ExampleController.php
+++ Classes/Controller/ExampleController.php
@@ -2,10 +2,12 @@
 
 namespace Alexgunkel\Typo3template\Controller;
 
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
 class ExampleController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
     public function indexAction()
     {
-
+        
     }
 }
