82058b339c716108163300ed91785e3a08d83292
Alexander Gunkel <alexandergunkel@gmail.com>
2017-01-08 23:37:45 +0100

    Add folders and change requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.4.99',
+            'typo3' => '7.6.0-8.9.99',
         ),
         'conflicts' => array(
         ),
